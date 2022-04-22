70fbae4ffb5454378aee4cca2f4d90dc3b1a1881
BakPeter <bak.pet@gmail.com>
2022-04-22 08:52:14 +0300

    commit 7

--- deployment-01-starting-setup/app.js
+++ deployment-01-starting-setup/app.js
@@ -1,13 +1,14 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'welcome.html');
+app.get("/", (req, res) => {
+  console.log("get exzmple-1");
+  const filePath = path.join(__dirname, "views", "welcome.html");
   res.sendFile(filePath);
 });
 
